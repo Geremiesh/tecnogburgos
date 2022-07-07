97f43c2c5dc2c635f148956de10d3fa4a68e4796
Geremiesh <geremiias@gmail.com>
2022-07-07 00:09:57 -0300

    subiendo archivo al repositorio

---
+++ my-app/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const name = 'TecnoG';
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          bienvenidos a la pagina! 
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {name}
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
