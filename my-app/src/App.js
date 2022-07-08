978bcde15a11d57a278e669f02eed75f9a6d3955
Geremiesh <geremiias@gmail.com>
2022-07-08 11:18:28 -0300

    Subiendo archivos a git

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          bienvenidos a la pagina! 
+          bienvenidos a la pagina!. 
         </p>
         <a
           className="App-link"
