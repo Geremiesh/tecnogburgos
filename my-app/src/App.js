eaacbef26beae5886ae8f8b3d6a04c245ef727c7
Geremiesh <geremiias@gmail.com>
2022-07-18 22:17:07 -0300

    haciendo cambios tp

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,7 +9,22 @@ function App() {
 
   return (
     <div className="App">
-      <NavBar/>
+      <div className='contenedor-principal'>
+        <div>
+            <p className='descuento-nav container'>DESCUENTO DEL %10 ABONANDO EFECTIVO/TRANSFERENCIA. ENVÍO GRATIS A SUCURSAL DE CORREO ARGENTINO EN
+                COMPRAS
+                SUPERIORES A $6000</p>
+              <div>
+                <ul className='contenedor-solapa'>
+                <img className='imagen-navbar' src={require('./Imagenes/tecno.png')} alt='Tecnog-logo'/>
+                  <li>INICIO</li>
+                  <li>PRODUCTOS</li>
+                  <li>CONTACTO</li>
+                </ul>
+              </div>
+        </div>
+          <NavBar/>
+      </div>
     </div>
   );
 }
--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -4,33 +4,11 @@ import '../style/NavBar.css';
 function NavBar() {
     return (
 
-    <div className='contenedor-navbar'>
-            <p className='descuento-nav'>DESCUENTO DEL %10 ABONANDO EFECTIVO/TRANSFERENCIA. ENVÍO GRATIS A SUCURSAL DE CORREO ARGENTINO EN
-                COMPRAS
-                SUPERIORES A $6000</p>
-                <ul className='contenedor-palabras'>
-                <li>INICIO</li>
-                <li>PRODUCTOS</li>
-                <li>CONTACTO</li>
-                </ul>
-            <div className='container'>
-                <img src='https://http2.mlstatic.com/D_NQ_NP_783184-MLA47545708641_092021-O.webp' className='imagen-logo' alt=''></img>
-                <div>
-                    <h5>AURICULARES GAMER</h5>
-                <a href='#' className='btn btn-success'>COMPRAR</a>
-                </div>
-            </div>
+    <div>
+      
     </div>
     );
 }
-
-
-
-
-
-
-
-
 
 
 
